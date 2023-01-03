f1df1d28f4ce3add14b7f54277577e6b8fdd59d0
TrongBinhNG <106975710+TrongBinhNG@users.noreply.github.com>
2023-01-03 15:50:32 +0700

    Update kadai2.rb

--- kadai2.rb
+++ kadai2.rb
@@ -7,15 +7,14 @@ class Menu
   end
 end
 
-# Define menu items
+
 menu1 = Menu.new("Phở", "30,000vnd")
 menu2 = Menu.new("Bún chả", "40,000vnd")
 menu3 = Menu.new("Bánh mì", "20,000vnd")
 
-# Put menu items in an array
+
 menus = [menu1, menu2, menu3]
 
-# Iterate over the menus and print them out
 menus.each do |menu|
   puts "#{menu.name} #{menu.price}"
 end
